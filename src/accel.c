a87a32c775fcca4a466eec16b9d1f6126f74b1c6
Alexandr Kolosov <rikorsev@gmail.com>
2021-09-29 00:37:39 +0300

    Implement accel sample rate change via i2c. Not tested yet.

--- src/accel.c
+++ src/accel.c
@@ -5,18 +5,24 @@
 #include <storage/flash_map.h>
 #include <stdio.h>
 #include <logging/log.h>
-#include <drivers/gpio.h>
+#include <drivers/i2c.h>
 
 #include "accel.h"
 
 LOG_MODULE_REGISTER(accel);
 
-#define ACCEL           DT_LABEL(DT_INST(0, invensense_mpu6050))
-#define PERIOD          K_MSEC(100)
-#define QUEUE_SIZE      10
-#define QUEUE_TIMEOUT   K_MSEC(100)
+#define ACCEL                DT_LABEL(DT_INST(0, invensense_mpu6050))
+#define I2C                  DT_LABEL(DT_NODELABEL(i2c0))
+#define PERIOD               K_MSEC(100)
+#define QUEUE_SIZE           10
+#define QUEUE_TIMEOUT        K_MSEC(100)
+#define SAMPLE_RATE_DEVIDER  99
+#define SAMPLE_RATE_REG_ADDR 0x19
+#define I2C_ACCEL_ADDRESS    0x68
+#define DLPF_VALUE           6
 
 static const struct device *accle_device;
+static const struct device *i2c_device;
 static bool is_running = false;
 static uint32_t base_timestamp = 0; // Timestamp whene record was started
 static uint16_t count = 0;
@@ -108,10 +114,32 @@ bool accel_is_running(void)
     return is_running;
 }
 
+int accel_sample_rate_set(const struct device *i2c)
+{
+    /* Switch on Digital Low-Pass filter (DLPF) to decrease sample rate to 1kHz */
+    /* Set sample rate devider */
+    uint8_t data[] = { SAMPLE_RATE_REG_ADDR, SAMPLE_RATE_DEVIDER, DLPF_VALUE };
+	struct i2c_msg message = {0};
+
+    message.buf   = data;
+	message.len   = sizeof(data);
+	message.flags = I2C_MSG_WRITE | I2C_MSG_STOP;
+
+    return i2c_transfer(i2c, &message, 1, I2C_ACCEL_ADDRESS);
+}
+
 void accel_init(void)
 {
-    accle_device = device_get_binding(ACCEL);
-    __ASSERT(accle_device != NULL, "Failed to find sensor %s", ACCEL);
+    int result = 0;
 
+    accle_device = device_get_binding(ACCEL);
+    __ASSERT(accle_device != NULL, "Failed to find %s", ACCEL);
+
+    i2c_device = device_get_binding(I2C);
+    __ASSERT(accle_device != NULL, "Failed to find %s", I2C);
+
+    result = accel_sample_rate_set(i2c_device);
+    __ASSERT(result == 0, "Failed to set accel sample rate. Result %d", result);
+    
     LOG_INF("Inited successfully");
 }
